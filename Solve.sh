a75f1c7419cf5427930b0e303c7cce2e57cb207d
Alyssa Byrnes <alyssa@alyssas-mbp.lan>
2022-08-10 10:45:25 -0400

    Updated shell scripts to run 3x3 example.

--- Solve.sh
+++ Solve.sh
@@ -1,1 +1,1 @@
-../MADP/src/solvers/GMAA --sparse --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob  -Q QMDP --useQcache 33gw-sharedcontrol.dpomdp -h2
+../MADP/src/solvers/GMAA --sparse --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob  -Q QMDP --useQcache 33gw.dpomdp -h2

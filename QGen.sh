79a50998c2d27a1c7f7b4c69d34279a28dc7757b
Alyssa Byrnes <alyssa@Alyssas-MacBook-Pro.local>
2022-08-10 10:53:19 -0400

    Updated horizon in shell calls from 2 to 4

--- QGen.sh
+++ QGen.sh
@@ -1,2 +1,2 @@
 cd ../MADP/src/utils
-./calculateQheuristic ~/public/alyssadpomdp/DecPOMDPGridworld/33gw.dpomdp -h2 -Q QMDP
+./calculateQheuristic ~/public/alyssadpomdp/DecPOMDPGridworld/33gw.dpomdp -h4 -Q QMDP

40d3894fd0b4246a37df00d53d281aefeafdcce8
Alyssa Byrnes <alyssa@Alyssas-MacBook-Pro.local>
2022-08-17 15:59:46 -0400

    Updated shell script to call all 3 examples

--- QGen.sh
+++ QGen.sh
@@ -1,2 +1,4 @@
 cd ../MADP/src/utils
 ./calculateQheuristic ~/public/alyssadpomdp/DecPOMDPGridworld/33gw.dpomdp -h4 -Q QMDP
+./calculateQheuristic ~/public/alyssadpomdp/DecPOMDPGridworld/33gw-nocomm.dpomdp -h4 -Q QMDP
+./calculateQheuristic ~/public/alyssadpomdp/DecPOMDPGridworld/33gw-late.dpomdp -h4 -Q QMDP
--- Solve.sh
+++ Solve.sh
@@ -1,1 +1,3 @@
 ../MADP/src/solvers/GMAA --sparse --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob  -Q QMDP --useQcache 33gw.dpomdp -h4
+../MADP/src/solvers/GMAA --sparse --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob  -Q QMDP --useQcache 33gw-nocomm.dpomdp -h4
+../MADP/src/solvers/GMAA --sparse --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob  -Q QMDP --useQcache 33gw-late.dpomdp -h4

5e94dccd8a81ad3271c13d838c7aacde70518dbc
alyssabyrnes <alyssanbyrnes@gmail.com>
2022-09-17 16:10:07 -0400

    Updated solve shell script to include timing

--- Solve.sh
+++ Solve.sh
@@ -1,3 +1,11 @@
+now="$(date +'%M')"
+echo "Start time ex 1: $now"
 ../MADP/src/solvers/GMAA --sparse --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob  -Q QMDP --useQcache 33gw.dpomdp -h4
+now="$(date +'%M')"
+echo "Start time ex 2: $now"
 ../MADP/src/solvers/GMAA --sparse --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob  -Q QMDP --useQcache 33gw-nocomm.dpomdp -h4
+now="$(date +'%M')"
+echo "Start time ex 3: $now"
 ../MADP/src/solvers/GMAA --sparse --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob  -Q QMDP --useQcache 33gw-late.dpomdp -h4
+now="$(date +'%M')"
+echo "End time ex 3: $now"

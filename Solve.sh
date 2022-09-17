bb74d6df5389bf3879943548885a8211951b031f
alyssabyrnes <alyssanbyrnes@gmail.com>
2022-09-17 16:26:47 -0400

    updated shell script

--- Solve.sh
+++ Solve.sh
@@ -1,11 +1,11 @@
 now="$(date +'%M:%S')"
 echo "Start time ex 1: $now"
-../MADP/src/solvers/GMAA --sparse --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob  33gw.dpomdp -h4 >
+../MADP/src/solvers/GMAA --sparse --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob  33gw.dpomdp -h4 > /dev/null
 now="$(date +'%M:%S')"
 echo "Start time ex 2: $now"
-../MADP/src/solvers/GMAA --sparse --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob 33gw-nocomm.dpomdp -h4 > 
+../MADP/src/solvers/GMAA --sparse --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob 33gw-nocomm.dpomdp -h4 > /dev/null
 now="$(date +'%M:%S')"
 echo "Start time ex 3: $now"
-../MADP/src/solvers/GMAA --sparse --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob  33gw-late.dpomdp -h4 > 
+../MADP/src/solvers/GMAA --sparse --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob  33gw-late.dpomdp -h4 > /dev/null
 now="$(date +'%M:%S')"
 echo "End time ex 3: $now"

4c2486610d9e4c99a1f24836ec8f7e16f5b839a6
alyssabyrnes <alyssanbyrnes@gmail.com>
2022-09-17 16:16:42 -0400

    updated shell script

--- Solve.sh
+++ Solve.sh
@@ -1,11 +1,11 @@
 now="$(date +'%M:%S')"
 echo "Start time ex 1: $now"
-../MADP/src/solvers/GMAA  33gw.dpomdp -h4 > gw.log
+../MADP/src/solvers/GMAA --sparse --GMAA=MAAstar 33gw.dpomdp -h4 > gw.log
 now="$(date +'%M:%S')"
 echo "Start time ex 2: $now"
-../MADP/src/solvers/GMAA 33gw-nocomm.dpomdp -h4 > gw2.log
+../MADP/src/solvers/GMAA --sparse --GMAA=MAAstar 33gw-nocomm.dpomdp -h4 > gw2.log
 now="$(date +'%M:%S')"
 echo "Start time ex 3: $now"
-../MADP/src/solvers/GMAA  33gw-late.dpomdp -h4 > gw3.log
+../MADP/src/solvers/GMAA --sparse --GMAA=MAAstar 33gw-late.dpomdp -h4 > gw3.log
 now="$(date +'%M:%S')"
 echo "End time ex 3: $now"

9104ac6a87729f2e90bad08e7b7d02d2646f904d
alyssabyrnes <alyssanbyrnes@gmail.com>
2022-09-19 13:18:01 -0400

    Modified shell scripts

--- Solve.sh
+++ Solve.sh
@@ -1,1 +1,1 @@
-../MADP/src/solvers/GMAA --sparse --GMAA=MAAstar --useBGclustering --BGIP_Solver=BnB --BnB-ordering=Prob  33gw-late.dpomdp -h4 
+../MADP2/src/solvers/GMAA --sparse --GMAA=MAAstar --useBGclustering --BGIP_Solver=BnB --BnB-ordering=Prob  33gw-late.dpomdp -h4 
